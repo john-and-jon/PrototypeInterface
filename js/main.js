bc103cd293b9a5b81e797f2ccfa0fefdcde12f25
Graphic <graphic@SJH510-05M.local>
2019-02-20 14:52:33 -0500

    feb_20
    In class moderation

--- js/main.js
+++ js/main.js
@@ -1,10 +1,27 @@
-let name{
-  first: null;
-  Last: null;
-}
+// let name{
+//   first: null;
+//   Last: null;
+// }
+//
+// let date{
+//   month: null;
+//   day: null;
+//   year: null;
+// }
 
-let date{
-  month: null;
-  day: null;
-  year: null;
-}
+
+let person_a = [{
+
+  name: `Rocco P`,
+  date: `June 1st`,
+
+  exercises: [
+  { name: "Push Ups", sets: 4, reps: 8 },
+  { name: "Air Squats", sets: 3, reps: 8},
+  { name: "Triceps Dips", sets: 3, reps: 10},
+  { name: "McGill Crunches", sets: 4, reps: 6},
+  { name: "Bird-Dogs", sets: 5, reps: 6},
+
+]
+
+},];

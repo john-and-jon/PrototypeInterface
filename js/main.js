be05a38f7be644a672fe7be3e2ee86eb0ac78291
John <johnsargant@hotmail.com>
2019-03-06 14:55:47 -0500

    JOHN
    I am cooler

--- js/main.js
+++ js/main.js
@@ -10,28 +10,32 @@
 // }
 
 
-let person_a = [
-  {
+//let person_a = {[
 
-  starting date: {month: June;,
-                  day: 7;,
-                  year: 1991}
 
-  name: { first: null;,
-        last: null;
-      },
-  date: { month: null;,
-         day: null;,
-         year: null;
-       },
+//  startingDate = { month: 06;,
+//                   day: 7;,
+//                   year: 1991}
 
-  exercises: [
+//  name = { first: null;,
+//        last: null;
+//      },
+  //date = { month: null;,
+  //       day: null;,
+  //       year: null;
+//       },
+//];}
+
+  var exercises = [
   { name: "Push Ups", sets: 4, reps: 8 },
   { name: "Air Squats", sets: 3, reps: 8},
   { name: "Triceps Dips", sets: 3, reps: 10},
   { name: "McGill Crunches", sets: 4, reps: 6},
   { name: "Bird-Dogs", sets: 5, reps: 6},
-
 ]
 
-},];
+
+
+
+console.log(exercises);
+document.getElementById('finished').innerHTML = exercises.map(ex => `<li>${ex.name}, ${ex.sets} sets, ${ex.reps} reps</li>`).join('');

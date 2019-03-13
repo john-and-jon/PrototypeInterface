4f5b8d51ae0e89cfa9478099f393876b5a35e5b7
Graphic <graphic@SJH510-04M.local>
2019-03-13 14:07:53 -0400

    Checklist
    I set up a checklist

--- js/main.js
+++ js/main.js
@@ -27,15 +27,21 @@
 //];}
 
   var exercises = [
-  { name: "Push Ups", sets: 4, reps: 8 },
-  { name: "Air Squats", sets: 3, reps: 8},
-  { name: "Triceps Dips", sets: 3, reps: 10},
-  { name: "McGill Crunches", sets: 4, reps: 6},
-  { name: "Bird-Dogs", sets: 5, reps: 6},
+  { id: 0,name: "Push Ups", sets: 4, reps: 8, complete: false},
+  { id: 1,name: "Air Squats", sets: 3, reps: 8, complete: true},
+  { id: 2,name: "Triceps Dips", sets: 3, reps: 10, complete: true},
+  { id: 3,name: "McGill Crunches", sets: 4, reps: 6, complete: false},
+  { id: 4,name: "Bird-Dogs", sets: 5, reps: 6, complete: true},
 ]
-
-
-
+//`<label>
+//<input type="checkbox" name="exercise${ex.id}">
+//</li>`
 
 console.log(exercises);
-document.getElementById('finished').innerHTML = exercises.map(ex => `<li>${ex.name}, ${ex.sets} sets, ${ex.reps} reps</li>`).join('');
+document.getElementById('finished').innerHTML = exercises.map(ex => `<li class="task"><label>
+<input type="checkbox" name="exercise${ex.id}">
+${ex.name}, ${ex.sets} sets, ${ex.reps} reps</li>`).join('');
+
+//let complete;
+//(complete) ? 'complete' : '';
+//(complete) ? 'checked' : '';
